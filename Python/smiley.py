c68e4af163ac40101ba56faf7d658fb3665e5914
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-03-09 14:18:27 +0500

    Learning oop

---
+++ Python/smiley.py
@@ -0,0 +1,23 @@
+class Smiley():
+
+    def __init__(self):
+        self.is_smile = True
+        self.face_color = "white"
+        self.eye_color = "white"
+
+    def set_smile(self, smile_status):
+        is_smile = smile_status
+        return None
+    
+    def set_face_color():
+        pass
+
+    def set_eye_color():
+        pass
+
+happy = Smiley()
+happy.is_smile
+
+sad = Smiley()
+sad.set_smile(False)
+sad.is_smile
