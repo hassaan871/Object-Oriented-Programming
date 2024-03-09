3b3faf2a838e5bfd65961193e256ba343169329c
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-03-09 12:07:50 +0500

    playing with variables

--- Python/Variables.py
+++ Python/Variables.py
@@ -3,4 +3,25 @@ var1 = "string"
 var2 = ['list', 1, True]
 var3 = {"one":1, "two":2}
 
-print("values of variable are {} {} {} {}".format(variable_name,var1,var2,var3))
+# print("values of variable are {} {} {} {}".format(variable_name,var1,var2,var3))
+
+string = "our string is this in the double quotes"
+# print(string[0])
+# print(string[26])
+# print(string[0:10])
+
+list_1 = [1,2,3]
+list_1.append(4)
+# print(list_1)
+
+number_of_rows = 3
+number_of_columns = 4
+matrix_list = [[0]*number_of_columns]*number_of_rows
+# print(matrix_list)
+
+tuple_1 = (1,2,3)
+# print(tuple_1)
+
+dictionary_1 = {"one":1, "two":2}
+print(dictionary_1["one"])
+print(dictionary_1["two"])
