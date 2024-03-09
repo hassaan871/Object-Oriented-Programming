6da89891bfc079c529f8ab2d6f02202428f64372
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-03-09 12:14:43 +0500

    Playing with arithmetic operators

---
+++ Python/arithmetic_operators.py
@@ -0,0 +1,16 @@
+a = 15
+b = 7
+
+add = a+b
+subtract = a-b
+divide = a/b
+power = a**b
+modulos = a%b
+lower_divide = a//b
+
+print(add)
+print(subtract)
+print(divide)
+print(power)
+print(modulos)
+print(lower_divide)
